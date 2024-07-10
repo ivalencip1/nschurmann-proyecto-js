dcad269dbea1fe7f99fcad90b06ae7870bb4d631
isabela v <ivalencip1@eafit.edu.co>
2024-07-10 09:51:37 -0500

    guardando cambios

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const fn = () => {
     const miString = "soy un string!"
     let b = miString + "muy feliz"
     console.log(b)
+    //control b para ocultar el menu bar
+    //control p para la paleta de comandos
+    //control k, sulto la k para poner la p (sin soltar control) para ver los archivos abiertos
+    // control shift e, para cambiar entre archivos
+
 }
 
 fn()
